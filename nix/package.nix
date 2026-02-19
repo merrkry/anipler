0e0eb6e4858ea4adc610a9ada577ae2557f84be1
Merrkry <merrkry@tsubasa.moe>
2026-02-19 10:15:12 +0100

    nix: fix cargo deps hash

--- nix/package.nix
+++ nix/package.nix
@@ -22,7 +22,7 @@ rustPlatform.buildRustPackage rec {
   # but it fetches EVERY dependency as separate FOD, which feels scary.
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
-    hash = "sha256-Y9AxOc37iDRYex6tNccGTytLvgvt3+9En3/wvzdHUCI=";
+    hash = "sha256-o2uJxJlMLqAQ1SUUhu10+Sg5AmmO1t8buC+fa/lI+m0=";
   };
 
   nativeBuildInputs = [
