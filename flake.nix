e2ac801f3e4585f081abfbbe7c3ec2c8e2b855fa
Merrkry <merrkry@tsubasa.moe>
2026-04-16 13:44:42 +0200

    build: migrate to static linking
    - Drop direnv, since .so are no longer needed
    - Use mise for dev environment management
    - Prefer bundled / pure rust dependency

--- flake.nix
+++ flake.nix
@@ -20,35 +20,6 @@
 
     in
     {
-      devShells = forAllSystems (
-        { pkgs }:
-        let
-          linkedLibs = with pkgs; [
-            openssl
-            sqlite
-          ];
-        in
-        {
-          default = pkgs.mkShell {
-            packages =
-              (with pkgs; [
-                pkg-config
-                rustPlatform.bindgenHook
-              ])
-              ++ linkedLibs;
-
-            env = {
-              RUST_LOG = "info,anipler=debug";
-            };
-
-            # Sqlx's proc-macros somehow failed to find libsqlite3.so. etc.
-            shellHook = ''
-              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath linkedLibs}:$LD_LIBRARY_PATH"
-            '';
-          };
-        }
-      );
-
       nixosModules.default = import ./nix/nixos.nix;
 
       packages = forAllSystems (
